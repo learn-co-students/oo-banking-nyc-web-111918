0a025166d608dea63f6d229e3fa3ab2dcbd601b5
Damon Collins <damon.c.p.collins@gmail.com>
2018-11-23 13:04:49 -0500

    added bank account and transfer classes

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,36 @@
 class Transfer
-  # your code here
+  attr_reader :sender, :receiver
+  attr_accessor :amount, :status
+
+  def initialize(sender, receiver, amount)
+      @sender = sender
+      @receiver = receiver
+      @amount = amount
+      @status = "pending"
+  end
+
+  def valid?
+    @sender.valid? && @receiver.valid?
+  end
+
+  def execute_transaction
+    if  @sender.balance < @amount
+      @status = "rejected"
+      return "Transaction rejected. Please check your account balance."
+    end
+
+    unless @status == "complete"
+      @sender.balance -= @amount
+      @receiver.deposit(@amount)
+      @status = "complete"
+    end
+  end
+
+  def reverse_transfer
+    if @status == "complete"
+      @receiver.balance -= @amount
+      @sender.deposit(@amount)
+      @status = "reversed"
+    end
+  end
 end
